5656ab01aa4c749a830ff8b5574cfbffe2dfc45d
Adepoju Oluwatoyin <93817465+teewhy100@users.noreply.github.com>
2022-12-12 15:35:48 +0100

    Add files via upload

---
+++ Cars Listing.js
@@ -0,0 +1,12 @@
+var cars = ['toyota', 'honda', 'mazda', 'mitsubishi', 'tesla', 'lexus'];
+
+console.log(cars[0]);
+
+cars[cars.length - 1];
+
+cars[6] = 'mercedes-benz';
+
+cars.forEach(function (value,  index) {console.log(value,  index)
+    
+});
+
